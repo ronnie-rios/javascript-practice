9a9eeaca9caf9e5802964ce78db08cdaafaf3fd4
Ronnie Rios <ronniesmail14@gmail.com>
2022-06-27 11:39:07 -0500

    foreach

--- arr-methods/forEach.js
+++ arr-methods/forEach.js
@@ -5,3 +5,27 @@ function eatFruit(fruit) {
     const eat = `i'm eating a ` +fruit
     console.log(eat)
 }
+
+const items = [
+    {
+        name: 'momo',
+        id: 1
+    },
+    {
+        name: 'biscuit',
+        id: 2
+    },
+    {
+        name: 'ajax',
+        id: 3
+    },
+]
+//add property to obj
+items.forEach(addProp)
+function addProp(obj) {
+    obj.type = 'cat'
+}
+console.log(items)
+//refactor to one line
+items.forEach(obj => obj.type = 'cat')
+console.log(items)
