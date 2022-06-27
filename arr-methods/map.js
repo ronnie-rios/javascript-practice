0aab709bbdcc3c272c532312e01f433e89083682
Ronnie Rios <ronniesmail14@gmail.com>
2022-06-27 11:03:09 -0500

    map method

---
+++ arr-methods/map.js
@@ -0,0 +1,16 @@
+const items = [
+    {
+        name: 'momo',
+        id: 1
+    },
+    {
+        name: 'biscuit',
+        id: 2
+    },
+    {
+        name: 'ajax',
+        id: 3
+    },
+]
+
+items.map(item => console.log(item.name))
