8cc2f99570a4c881dfc3216144e1a8d572762dcc
Ronnie Rios <ronniesmail14@gmail.com>
2022-06-27 11:13:12 -0500

    comments for clarity

--- arr-methods/add-remove.js
+++ arr-methods/add-remove.js
@@ -12,8 +12,10 @@ const items = [
         id: 3
     },
 ]
-items.push({name: 'new-chicken', id: 4})
+//note push returns the length
+//to see what if item gets added to arr, check the array again
 console.log(items)
 
+//same goes for pop items.push({name: 'new-chicken', id: 4})
 items.pop()
 console.log(items)
--- arr-methods/find.js
+++ arr-methods/find.js
@@ -1,4 +1,4 @@
 const list = ['apple', 'pear', 'carrots','plum']
-
+//finds the element in the arr with a length greater than 6
 const findVegetable = list.find(ele => ele > 6)
 console.log(findVegetable)
