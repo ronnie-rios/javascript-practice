c7b7504054f1c02ea87d4ef42bab0600c8c827ab
Ronnie Rios <ronniesmail14@gmail.com>
2022-06-27 11:16:55 -0500

    pop push shift methods

--- arr-methods/add-remove.js
+++ arr-methods/add-remove.js
@@ -14,8 +14,17 @@ const items = [
 ]
 //note push returns the length
 //to see what if item gets added to arr, check the array again
+items.push({name: 'new-chicken', id: 4})
 console.log(items)
 
-//same goes for pop items.push({name: 'new-chicken', id: 4})
+//same goes for pop 
 items.pop()
+console.log(items)
+
+//shift
+//removes the first element in an arr and returns that removed element, changes length of arr
+const removedCat = items.shift()
+console.log(removedCat)
+
+//return the new arr after shift completed
 console.log(items)
