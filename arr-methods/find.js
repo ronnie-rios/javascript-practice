d51a3f55d2936ac28754743c0df0b5aa99dc4f02
Ronnie Rios <ronniesmail14@gmail.com>
2022-06-27 11:41:13 -0500

    find obj with certain property

--- arr-methods/find.js
+++ arr-methods/find.js
@@ -2,3 +2,20 @@ const list = ['apple', 'pear', 'carrots','plum']
 //finds the element in the arr with a length greater than 6
 const findVegetable = list.find(ele => ele > 6)
 console.log(findVegetable)
+
+const items = [
+    {
+        name: 'momo',
+        id: 1
+    },
+    {
+        name: 'biscuit',
+        id: 2
+    },
+    {
+        name: 'ajax',
+        id: 3
+    },
+]
+//finds the obj with the id of 1
+console.log(items.find(cat => cat.id === 1))
