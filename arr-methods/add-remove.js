540d4d285d7e58f6140b5ee9fa70d3b8f909957e
Ronnie Rios <ronniesmail14@gmail.com>
2022-06-27 11:11:30 -0500

    pop and push method

---
+++ arr-methods/add-remove.js
@@ -0,0 +1,19 @@
+const items = [
+    {
+        name: 'momo',
+        id: 1
+    },
+    {
+        name: 'biscuit',
+        id: 2
+    },
+    {
+        name: 'ajax',
+        id: 3
+    },
+]
+items.push({name: 'new-chicken', id: 4})
+console.log(items)
+
+items.pop()
+console.log(items)
