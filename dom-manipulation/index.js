32ac87538d80d64b47f9b747daad6865168a2fbd
Ronnie Rios <ronniesmail14@gmail.com>
2022-06-30 09:52:02 -0500

    dom manipulation prac

---
+++ dom-manipulation/index.js
@@ -0,0 +1,32 @@
+const data = [
+    {
+        id: 1,
+        name: 'momo',
+        hobbies: 'sleeping'
+    },
+    {
+        id: 2,
+        name: 'ajax',
+        hobbies: 'pink pudge'
+    },
+    {
+        id: 3,
+        name: 'bizkit',
+        hobbies: 'racing'
+    },
+]
+
+let button = document.getElementById('displayBtn')
+
+
+function displayData() {
+    data.forEach(cat => {
+        let todoList = document.getElementById('to-do')
+        let catNameEle = document.createElement('h2')
+        catNameEle.innerText = cat.name
+        catNameEle.append(todoList)
+    });
+}
+
+button.addEventListener("click", displayData(), console.log('clicked')) 
+
