237f7d42401b59d4cb01fe7a6115f3abd09c2403
Ronnie Rios <ronniesmail14@gmail.com>
2022-07-07 18:24:58 -0500

    pop and shift methods

---
+++ arr-methods/pop-shift.js
@@ -0,0 +1,6 @@
+let greetings = ['whats up?', 'hello', 'see ya!'];
+//removes last element and takes in  no parameters
+greetings.pop();
+//removes first element from the beginning
+greetings.shift();
+//both modify the array one elment at a time
