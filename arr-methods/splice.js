2b3b2bc0c70685c8f459209f2b1189086c9ed7fa
Ronnie Rios <ronniesmail14@gmail.com>
2022-07-07 18:41:29 -0500

    splice method with 3 params

--- arr-methods/splice.js
+++ arr-methods/splice.js
@@ -5,3 +5,22 @@ const arr = [2, 4, 5, 1, 7, 5, 2, 1];
 arr.splice(1,4)
 // Only change code above this line
 console.log(arr);
+
+const numbers = [10, 11, 12, 12, 15];
+//naming the parameters to see it better
+const startIndex = 3;
+const amountToDelete = 1;
+numbers.splice(startIndex, amountToDelete, 13, 14);
+// The index at which to begin deleting elements (3), the number of elements to be deleted (1),
+// and the remaining arguments (13, 14) will be inserted starting at that same index. 
+//Note that there can be any number of elements (separated by commas) following amountToDelete, each of which gets inserted.
+console.log(numbers);
+
+function htmlColorNames(arr) {
+    // Only change code below this line
+  arr.splice(0,2, 'DarkSalmon', 'BlanchedAlmond')
+    // Only change code above this line
+    return arr;
+  }
+  
+  console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
