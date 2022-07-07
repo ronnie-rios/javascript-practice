bfd8468032f6d030b23ffc8938824835d45cbfd6
Ronnie Rios <ronniesmail14@gmail.com>
2022-07-07 18:15:07 -0500

    splice.js

---
+++ arr-methods/splice.js
@@ -0,0 +1,7 @@
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+// Only change code below this line
+//will return an arr that adds up to 10
+//starts at 4 and removes 4 elements
+arr.splice(1,4)
+// Only change code above this line
+console.log(arr);
