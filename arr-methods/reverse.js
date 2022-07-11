5a35a196d8e37af11c73767413c9c0896abe258b
Ronnie Rios <ronniesmail14@gmail.com>
2022-07-11 07:54:38 -0500

    reverse

---
+++ arr-methods/reverse.js
@@ -0,0 +1,9 @@
+const arrCats = ['momo', 'ajax', 'bizkit']
+
+console.log(arrCats.reverse())
+
+const strCat = 'tikzib'
+const arrStrCat = strCat.split('')
+console.log(arrStrCat)
+
+console.log(arrStrCat.reverse().join(''))
