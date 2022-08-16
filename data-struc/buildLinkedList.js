ecd62a34772d2bb41c61cbde0a8b173ee5c8e0da
Ronnie Rios <ronniesmail14@gmail.com>
2022-08-16 10:20:44 -0500

    insert method

--- data-struc/buildLinkedList.js
+++ data-struc/buildLinkedList.js
@@ -64,7 +64,39 @@ class LinkedList {
         }
         return arr
     }
+    //first node is "leader"
+    //point to 'old node' that was originally conneted to leader
     insert(index, value) {
-
+        //check the length
+        if (index >= this.length) {
+            //add to the end
+            return this.append(value)
+        }
+        const newNode = new Node(value)
+        const leader = this.traversToIndex(index-1);
+        const holdPointer = leader.next;
+        leader.next = newNode;
+        newNode.next = holdPointer
+        this.length++
+        return this.printList()
+    }
+    traversToIndex(index) {
+        //loop though o(n)
+        let counter = 0;
+        let currentNode = this.head;
+        //go from head to counter till it = index
+        while(counter !== index) {
+            //keep moving the node
+            currentNode = currentNode.next
+            counter++
+        }
+        return currentNode;
     }
 }
+
+const myLinkedList = new LinkedList(15);
+myLinkedList.append(5)
+myLinkedList.prepend(3)
+myLinkedList.insert(1, 8)
+console.log(myLinkedList.printList())
+//console.log(myLinkedList);
