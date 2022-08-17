9062adb903cb9724774f1ab9d79513ab0eaa2ab1
Ronnie Rios <ronniesmail14@gmail.com>
2022-08-17 11:49:18 -0500

    anagram solution

--- algo/angram.js
+++ algo/angram.js
@@ -35,3 +35,24 @@ function buildCharMap(str) {
 console.log(anagrams('momo', 'moom'));
 
 //.replace(/[^\w/g,""])
+
+const isAnagram = function(s, t) {
+    if(s.length !== t.length) return false
+    const sCount ={}
+    const tCount = {}
+   
+    for(let i=0; i < s.length; i++) {
+        if(!sCount[s[i]]) {
+            sCount[s[i]] = 0
+        }
+        if(!tCount[t[i]]) {
+            tCount[t[i]] = 0
+        }
+        sCount[s[i]]++
+        tCount[t[i]]++
+    }
+    for (let char in sCount) {
+        if(sCount[char]!==tCount[char]) return false
+    }
+    return true
+};
