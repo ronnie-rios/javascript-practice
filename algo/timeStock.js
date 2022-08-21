d942d03092fe934b696ea9f410865a7b0e4de30f
Ronnie Rios <ronniesmail14@gmail.com>
2022-08-21 16:38:58 -0500

    stock solution

---
+++ algo/timeStock.js
@@ -0,0 +1,13 @@
+
+var maxProfit = function(prices) {
+    let minBuyPrice = prices[0]
+    let max = 0
+    for(let i = 1; i<prices.length; i++) {
+         const sellPrice = prices[i]
+         const  profit = sellPrice-minBuyPrice
+         max=Math.max(max, profit)
+         minBuyPrice = Math.min(minBuyPrice,prices[i])
+        console.log(sellPrice)
+    }
+    return max
+};
