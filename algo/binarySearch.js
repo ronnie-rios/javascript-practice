d6f4e29fb324f1af24687e198e8cd08400c1cd39
Ronnie Rios <ronniesmail14@gmail.com>
2022-08-23 09:44:59 -0500

    binary search

--- algo/binarySearch.js
+++ algo/binarySearch.js
@@ -2,3 +2,22 @@
 const search = function(nums, target) {
     return nums.indexOf(target)
 }
+
+var search2 = function(nums, target) {
+    let start = 0
+    let end = nums.length -1
+    
+    while(start <= end) {
+        let middle = Math.floor((start + end) /2)
+        
+        if(target ===nums[middle]) {
+            return middle
+        } else if (nums[middle] < target) {
+            start = middle + 1
+        } else {
+            end = middle -1
+        }
+        
+    }
+    return -1
+};
