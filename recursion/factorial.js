5a1975c5b3d451dc69d213b1a14d40a2ea91ebea
Ronnie Rios <ronniesmail14@gmail.com>
2022-08-23 14:02:34 -0500

    create factorial funcs

--- recursion/factorial.js
+++ recursion/factorial.js
@@ -1,9 +1,22 @@
 //5! = 5 * 4 * 3 * 2 * 1
 //factorial
-function findRecursive() {
-
+function findRecursive(num) {
+//set base
+//identify the recursive
+//return
+    if (num===0) {
+        return 1
+    }
+    return num * findRecursive(num-1)
 }
 
-function findIterative() {
-
+function findIterative(num) {
+    let result = num
+    while (num > 1) {
+        num--
+        result = result * num
+    }
+    return result
 }
+console.log(findRecursive(5))
+console.log(findIterative(5))
