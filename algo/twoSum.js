84b1cc936bf68c6195e0ee0f924440158f03433d
Ronnie Rios <ronniesmail14@gmail.com>
2022-08-25 10:13:30 -0500

    twosum

---
+++ algo/twoSum.js
@@ -0,0 +1,9 @@
+var twoSum = function(nums, target) {
+    for (let i =0; i < nums.length; i++) {
+        let hold = target - nums[i]
+        if( nums.includes(hold) && i != nums.indexOf(hold)) {
+            return [i, nums.indexOf(hold)]
+        }
+    }
+};
+
