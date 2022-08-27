35b47699e974ea514c1f508889410f4774aab19e
Ronnie Rios <ronniesmail14@gmail.com>
2022-08-27 14:41:40 -0500

    comment for clairty

--- sorting/selectionsort.js
+++ sorting/selectionsort.js
@@ -3,10 +3,12 @@ const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
 function selectionSort(arr) {
     for(let i = 0; i <arr.length; i++) {
+        //set curr index as minimum
         let min = i
         let temp =arr[i]
         for(let j=i+1; j <arr.length; j++) {
             if(arr[j] < arr[min]) {
+                //update min if current is lower than prev
                min = j
             }
         }
