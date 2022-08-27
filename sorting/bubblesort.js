4aabedae2b0684a518be3245a2ee1f6d53a9619c
Ronnie Rios <ronniesmail14@gmail.com>
2022-08-27 14:40:24 -0500

    sorting

--- sorting/bubblesort.js
+++ sorting/bubblesort.js
@@ -2,7 +2,7 @@ const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
 function bubbleSort(arr) {
     for (let i =0; i<arr.length; i++) {
-        for(letj=0; j <arr.length; j++) {
+        for(let j=0; j <arr.length; j++) {
             if (arr[j] > arr[j+1]) {
                 //swap numbers
                 let temp = arr[j]
---
+++ sorting/selectionsort.js
@@ -0,0 +1,18 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function selectionSort(arr) {
+    for(let i = 0; i <arr.length; i++) {
+        let min = i
+        let temp =arr[i]
+        for(let j=i+1; j <arr.length; j++) {
+            if(arr[j] < arr[min]) {
+               min = j
+            }
+        }
+        arr[i] =arr[min]
+        arr[min] = temp
+    }
+  return  arr
+}
+
+console.log(selectionSort(numbers));
