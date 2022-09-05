1ea0513fadac4ba4331d5ac5f4344ef90cec2a1b
Ronnie Rios <ronniesmail14@gmail.com>
2022-09-05 16:10:39 -0500

    create two num sum for array alternate solutions

--- algo/twoSum.js
+++ algo/twoSum.js
@@ -6,4 +6,30 @@ var twoSum = function(nums, target) {
         }
     }
 };
+function twoNumberSum(array, targetSum) {
+    // Write your code here.
+     for(let i =0; i <array.length; i++) {
+       first = array[i]
+       for(let j=1; j <array.length; j++) {
+         second = array[j]
+         if(first + second === targetSum && first!=second) {
+           return [first, second]
+         }
+       }
+       
+     }
+    return []
+  }
 
+  function twoNumberSum2(array, targetSum) {
+    // Write your code here.
+     const check = new Set()
+    for(let num of array) {
+      const found = targetSum - num
+      if(check.has(found)) {
+        return [num, found]
+      }
+      check.add(num)
+    } return []
+  }
+  
