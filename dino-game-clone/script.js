b119b17c43907a29a261b1ba3245ba77c8952963
Ronnie Rios <ronniesmail14@gmail.com>
2022-09-24 10:44:22 -0500

    get cacuts and dino position

--- dino-game-clone/script.js
+++ dino-game-clone/script.js
@@ -1,4 +1,5 @@
 const dino = document.getElementById('dino');
+const cactus = document.getElementById('cactus');
 
 function jump() {
     if(dino.classList != 'jump') {
@@ -8,6 +9,15 @@ function jump() {
         },250)
     }
 };
+
+let isAlive = setInterval(() => {
+    //check y position of our dino and cactus
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue('top'));
+    let cactusLeft = parseInt(window.getComputedStyle('cactus').getPropertyValue('left'));
+
+    
+}, 10)
+
 
 document.addEventListener('keydown', (event) => {
     jump();
