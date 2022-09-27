99b2e0194646a720355776089ee2dfe1a8e9c0bc
Ronnie Rios <ronniesmail14@gmail.com>
2022-09-27 08:45:30 -0500

    every palindrome check

--- algo/palindrome.js
+++ algo/palindrome.js
@@ -26,3 +26,14 @@ function checkPalindrome(first, second) {
 }
 
 checkPalindrome("ronnie","einnor")
+
+function palindrome2(str) {
+    //check
+    //every does a boolean check
+    return str.split('').every((char, index) => {
+        //for char compare to itself
+        return char === str[str.length - index - 1];
+    });
+};
+
+console.log(palindrome2('racecar'));
