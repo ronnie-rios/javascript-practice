f5df5982c84b4a6493123c80c7b57a274e5839f9
Ronnie Rios <ronniesmail14@gmail.com>
2022-10-10 12:45:10 -0500

    recursive steps solution

--- algo/steps.js
+++ algo/steps.js
@@ -21,3 +21,20 @@ function steps(n) {
 //else a space
 //log the stair
 steps(6)
+
+function recursiveSteps(n, row = 0, stair = '') {
+    //make sure to pass and stair in row too
+    if (n === row) {
+        return;
+    }
+    if (n === stair.length) {
+        console.log(stair);
+        return recursiveSteps(n, row + 1);
+    }
+    if (stair.length <= row) {
+        stair += '#'
+    } else {
+        stair += ' '
+    }
+    recursiveSteps(n, row, stair)
+}
