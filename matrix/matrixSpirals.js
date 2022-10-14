734561a0590fdd5525c07dbb9fc735225089964b
Ronnie Rios <ronniesmail14@gmail.com>
2022-10-13 19:15:42 -0500

    pseudocoding

--- matrix/matrixSpirals.js
+++ matrix/matrixSpirals.js
@@ -3,3 +3,16 @@
 //[[1, 2], 
 ///[4,3]
 //the nums increase in a spiral into the center clockwise
+
+//create empty arr called results
+//create a counter at 1
+//as long as start column <= end column & start row <= end row
+    //loop from start to end
+        //at results[start-row][i] assign counter
+        ///increment counter
+    //increment start row
+    //loop from start row to end row
+        //at results[i][end-column] assign counter
+        //increment counter
+    //decrement end row
+    //repeat
