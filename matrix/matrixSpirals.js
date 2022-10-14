237544c999b77ffe295949a3c264d57036bd8918
Ronnie Rios <ronniesmail14@gmail.com>
2022-10-13 19:51:11 -0500

    final solution

--- matrix/matrixSpirals.js
+++ matrix/matrixSpirals.js
@@ -14,8 +14,8 @@
     //loop from start row to end row
         //at results[i][end-column] assign counter
         //increment counter
-    //decrement end row
-    //repeat
+    //decrement end column
+    //repeat - each loop will construct a different part of the loop
 
     function matrix(n) {
         const results = [];
@@ -32,11 +32,38 @@
         let endRow = n - 1;
 
         while(startColumn <= endColumn && startRow <= endRow) {
-            
+            //first for loop will assemble top row
+            for(let i = startColumn; i <= endColumn; i++) {
+                //set our results, from 0 to n, and at the particlar index
+                //assign the counter
+                results[startRow][i] = counter;
+                counter ++;
+            }
+            startRow++;
+            //going to iterate from start row to 1 to n, and we flip the index because we are using the index first, then the column
+            //right column
+            for(let i = startRow; i <= endRow; i++) {
+                results[i][endColumn] = counter;
+                counter ++;
+            }
+            endColumn--;
+            //bottom row
+            for(let i = endColumn; i >= startColumn; i--) {
+                results[endRow][i] = counter;
+                counter++;
+            }
+            endRow--;
+            //start column
+            for(let i = endRow; i >= startRow; i--) {
+                results[i][startColumn] = counter;
+                counter++;
+            }
+            startColumn++;
         }
 
     }
 
+console.log(matrix(3))
 
     const arr =[];
     arr[3] = 'momo'
