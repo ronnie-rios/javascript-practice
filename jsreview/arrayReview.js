a9673a42180d758e23ba3c38ec4af1f5a9a7f146
Ronnie Rios <ronniesmail14@gmail.com>
2022-10-15 13:25:38 -0500

    arr creation

---
+++ jsreview/arrayReview.js
@@ -0,0 +1,9 @@
+// creating arrays
+//normal way 
+const nums = [1, 2, 3]
+
+//arr constructor
+const arrConstructor = new Array(1, 2, 3);
+//if single num passed in, creates an array with a fixed length of empty values
+
+const arrOfVersion = Arr.of(1, 2)
