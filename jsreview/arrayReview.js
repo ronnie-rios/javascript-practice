d78b802e0dfb7d7652351f24b63377504be46a40
Ronnie Rios <ronniesmail14@gmail.com>
2022-10-15 13:37:06 -0500

    reviewing simple methods

--- jsreview/arrayReview.js
+++ jsreview/arrayReview.js
@@ -7,3 +7,17 @@ const arrConstructor = new Array(1, 2, 3);
 //if single num passed in, creates an array with a fixed length of empty values
 
 const arrOfVersion = Arr.of(1, 2)
+
+//takes in an iterable and turns it into an arr
+//hi turns into ['h', 'i']
+const arrFrom = Array.from('hi')
+
+
+//add data to arrs
+const stuff = ['cup', 'desk'];
+stuff.push('mouse') //adds to end => returns length of arr
+stuff.unshift('coding') // adds to start => returns length of arr
+stuff.pop() //removes the last ele
+stuff.shift(); //removes the first
+
+stuff[1] = 'momo land'
