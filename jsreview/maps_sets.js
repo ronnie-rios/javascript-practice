291f4ef4fe0c2ecdfc01c153c275b12c342a2ea1
Ronnie Rios <ronniesmail14@gmail.com>
2022-10-24 21:12:26 -0500

    sets prac

--- jsreview/maps_sets.js
+++ jsreview/maps_sets.js
@@ -7,3 +7,13 @@
 //any data type for keys can be used
 //iterable with some special methods
 //order is guaranteed, duplicate kets are NOT allowed
+
+//SETs - only way to create a set => automatically empty from start
+const ids = new Set([1,2,3]);
+
+//since sets are unique, don't need to get an index, rather check if it HAS a value
+console.log(ids.has(2)) //will check if it has 2, returns true or false
+
+//sets values are unique
+
+//entries() returns an iterable
