b64d710d1ec251ece1c4ee797a0cfb237e622144
Ronnie Rios <ronniesmail14@gmail.com>
2022-10-25 14:15:54 -0500

    queue class

---
+++ data-struc/queue.js
@@ -0,0 +1,22 @@
+//data structs
+//ways of organizing info w/ optimal runtime complexity for adding and removing records
+
+//js has some natively, often questions asked about inferior data structs
+
+//queues, enqueuing = adding | dequeuing = removing
+//FIFO = first in first out
+
+class Queue {
+    //when new class created constructor will be built
+    constructor() {
+        //store the data for our queue
+        this.data = [];
+    }
+    //add to front
+    add(record) {
+        this.data.unshift(record);
+    }
+    remove() {
+       return this.data.pop()
+    }
+}
