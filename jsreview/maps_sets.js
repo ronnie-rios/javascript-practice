7f766fc08f223015038806cde5b3c9172c536abb
Ronnie Rios <ronniesmail14@gmail.com>
2022-10-24 21:05:43 -0500

    maps and sets intro

---
+++ jsreview/maps_sets.js
@@ -0,0 +1,9 @@
+//SETs are data of any kind of length
+//iterable with some SET methods
+//order is not guaranteed, duplicates are NOT allowed, no index based access
+
+//MAPs
+//store key-value data of any kind and length
+//any data type for keys can be used
+//iterable with some special methods
+//order is guaranteed, duplicate kets are NOT allowed
