79c0fa65990dd26421855663b7b8d7964b164957
Ronnie Rios <ronniesmail14@gmail.com>
2022-10-25 14:22:33 -0500

    queues

---
+++ data-struc/queues/queue-weave.js
@@ -0,0 +1,7 @@
+//make a peek
+//implement the weave as arguments and combines the contents of
+// each into a new third queue
+//the third queue should containthe alterating content of 
+// the two queues, functions should handle queues of diff lengths
+//witout inserting
+//DO NOT access the array inside of a queue, use only add, remove, peek
