60e692cfbe9da72d91977327dbd23290bd99519b
Ronnie Rios <ronniesmail14@gmail.com>
2022-10-28 08:17:04 -0500

    loop review .js

---
+++ jsreview/loopRev.js
@@ -0,0 +1,13 @@
+const randomNumber = Math.random(); // produces random number between 0 (including) and 1 (excluding)
+
+const nums = [1,10,8,3,5,7,9,11,13,15,2]
+
+function basicLoop(nums) {
+    for(const num in nums) {
+        console.log(nums[num],'basic normal loop')
+    }
+    for(let i=nums.length-1; i >=0; i--) {
+        console.log(nums[i],'backwards')
+    }
+}
+basicLoop(nums)
