7314ecb482312cc9edb2ac8d21044f235aa7564a
Ronnie Rios <ronniesmail14@gmail.com>
2022-10-29 18:27:25 -0500

    backend up

--- errhandling/backend/app.js
+++ errhandling/backend/app.js
@@ -1,1 +1,22 @@
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const port = 9009;
+
+app.use(cors());
+
+const datacontroller = require('./controller/datacontroller');
+app.use('/data', datacontroller);
+
+app.get('/',(req,res) => {
+    res.json({ msg: 'backend'})
+});
+
+app.use((req, res) => {
+    res.json({ err: 'not found'})
+});
+
+app.listen(port, () => {
+    console.log(`port up on ${port}`)
+});
--- errhandling/backend/data/data.js
+++ errhandling/backend/data/data.js
@@ -1,4 +1,4 @@
-module.exports = data [
+module.exports = [
     {
         id: 1,
         name: "biscuit",
