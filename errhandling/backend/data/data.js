6f564d7c43333301fe85423a3408a0fbb7d6f83f
Ronnie Rios <ronniesmail14@gmail.com>
2022-10-29 18:15:52 -0500

    mini app backend

---
+++ errhandling/backend/data/data.js
@@ -0,0 +1,22 @@
+module.exports = data [
+    {
+        id: 1,
+        name: "biscuit",
+    },
+    {
+        id: 2,
+        name: "momo",
+    },
+    {
+        id: 3,
+        name: "ajax",
+    },
+    {
+        id: 4,
+        name: "momozingo",
+    },
+    {
+        id: 5,
+        name: "bizkonius",
+    }
+]
