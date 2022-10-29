60f32751d60d6ec177571555d213bdb79e0374dc
Ronnie Rios <ronniesmail14@gmail.com>
2022-10-29 18:03:18 -0500

    err handler

--- errhandling/index.js
+++ errhandling/index.js
@@ -1,5 +1,9 @@
 const fetchContainer = document.getElementById('err3-fetch');
 const fetchBtn = document.getElementById('fetch-btn');
+const username = document.getElementById('username').value;
+const password = document.getElementById('password').value;
+const form = document.getElementById('form');
+const formErr = document.getElementById('err2');
 
 async function getData() {
     try {
@@ -18,4 +22,20 @@ async function getData() {
     }
 }
 
+function submitHandler(e) {
+    e.preventDefault();
+    let errMessages = [];
+    if(password.length <5) {
+        errMessages.push('pw must be at least 6 chars')
+    }
+    if (errMessages.length > 0) {
+        e.preventDefault();
+        formErr.innerText = errMessages.join(', ')
+        setTimeout(() => {
+            formErr.innerText = '';
+        }, 2000)
+    }
+}
+
+form.addEventListener('submit', submitHandler)
 fetchBtn.addEventListener('click', getData)
