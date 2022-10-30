2c545c2ff622a33acaf8f6093bf20e22e02d8ddc
Ronnie Rios <ronniesmail14@gmail.com>
2022-10-29 19:03:53 -0500

    err handling refactor

--- errhandling/backend/controller/datacontroller.js
+++ errhandling/backend/controller/datacontroller.js
@@ -6,5 +6,14 @@ router.get('/', (req, res) => {
     res.json(data)
 });
 
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    const foundData = data.filter(item => item.id == id);
+    if (foundData < 1) {
+        res.json({ err: 'not found' })
+    } else {
+        res.json(foundData[0])
+    }
+});
 
 module.exports = router;
--- errhandling/frontend/index.js
+++ errhandling/frontend/index.js
@@ -5,24 +5,26 @@ const password = document.getElementById('password').value;
 const form = document.getElementById('form');
 const formErr = document.getElementById('err2');
 
-async function getData() {
+async function getData(e) {
     try {
-        let response = await fetch('url');
-        let useData = await response.json()
-        const dataTitle = document.createElement('h2');
-        dataTitle.innerText = useData.title
+        let response = await fetch('http://localhost:9009s/data/');
+        let useData = await response.json();
+        useData.forEach(item => {
+            createDomData(item)
+        })
     } catch (error) {
-        const errMsg = document.createElement('h2');
-        errMsg.setAttribute('id', 'err');
-        errMsg.innerText = error;
-        fetchContainer.append(errMsg);
-        setTimeout(() => {
-            errMsg.innerText = '';
-        }, 2000)
+        errHandler(e, error)
     }
+};
+
+const createDomData = (data) => {
+    const dataTitle = document.createElement('h2');
+    dataTitle.innerText = 'name: ' + data.name;
+    fetchContainer.append(dataTitle);
+    fetchBtn.setAttribute('hidden', true)
 }
 
-function submitHandler(e) {
+function errHandler(e, error) {
     e.preventDefault();
     let errMessages = [];
     if(password.length <5) {
@@ -31,11 +33,20 @@ function submitHandler(e) {
     if (errMessages.length > 0) {
         e.preventDefault();
         formErr.innerText = errMessages.join(', ')
-        setTimeout(() => {
-            formErr.innerText = '';
-        }, 2000)
+        setTimeout(errTimeout(formErr), 2000)
     }
+    if (error) {
+        const errMsg = document.createElement('h2');
+        errMsg.setAttribute('id', 'err');
+        errMsg.innerText = 'err!';
+        fetchContainer.append(errMsg);
+        setTimeout(errTimeout(errMsg), 2000)
+    }
+};
+
+function errTimeout(ele) {
+    ele.innertext=''
 }
 
-form.addEventListener('submit', submitHandler)
+form.addEventListener('submit', errHandler)
 fetchBtn.addEventListener('click', getData)
