3c6dbebf1c0b0fc3dd6eba12b3e1c78aa42ff825
Ronnie Rios <ronniesmail14@gmail.com>
2022-12-18 18:19:43 -0600

    promises example

---
+++ promises/promise.js
@@ -0,0 +1,20 @@
+const userInput = process.argv[2];
+
+const worldCup = (input) => {
+    return new Promise((resolve, reject) => {
+        if(input==='france') {
+            resolve('mbappe hat-trick')
+        } else if (input ==='argentina'){
+            resolve('messi goated')
+        } else {
+            reject('u dont know football')
+        }
+    })
+}
+
+worldCup(userInput).then(console.log).catch(console.log)
+console.log('after the promise, line 16');
+console.log('after the promise, line 17');
+console.log('after the promise, line 18');
+console.log('after the promise, line 19');
+console.log('after the promise, line 20');
