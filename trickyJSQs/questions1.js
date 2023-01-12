618bebaed297e4c54204abc8e3a321d6a3ee9521
Ronnie Rios <ronniesmail14@gmail.com>
2023-01-12 08:18:24 -0600

    questions

---
+++ trickyJSQs/questions1.js
@@ -0,0 +1,23 @@
+console.log([]+[]); //yields an empty string, why because empty arr trns into an empty str
+
+function a(){
+    return 'hello'
+}
+
+const sentence = a('hi');
+console.log(sentence);
+//returns hello because the func doesn't take any arguments
+
+function y(){
+    console.log(this.length);
+}
+
+var x = {
+    length: 5,
+    method: function(y) {
+        arguments[0]()
+    }
+}
+
+x.method(y, 1) //calls the x obj and the method, which takes in y and 1, which executes
+//y, and whatever y gives this.length but due to argments it points to length of the arguments
