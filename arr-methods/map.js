296a110690deca5bdec2adb878ad2ef87573d454
Ronnie Rios <ronniesmail14@gmail.com>
2023-01-19 15:47:11 -0600

    edits

--- arr-methods/map.js
+++ arr-methods/map.js
@@ -1,3 +1,4 @@
+
 const items = [
     {
         name: 'momo',
@@ -17,3 +18,12 @@ items.map(item => console.log(item.name))
 
 
 items.map(item => item.id = Math.random())
+nums = [1, 2, 3, 4];
+
+let a = [];
+console.log(a);
+const newArray = nums.forEach((num) => {
+  a.push(num);
+  
+});
+console.log(a);
--- array-practice-2/removing.js
+++ array-practice-2/removing.js
@@ -15,3 +15,16 @@ flatArr.forEach((cat) => {
     cat += ' is the best'
     console.log(cat)
 })
+
+
+
+function x(array, prop) {
+    return array.sort((a,b) => {
+        if(a[prop] < b[prop]) return -1
+        if(a[prop] > b[prop]) return 1
+
+        return 0
+    })
+}
+
+x
