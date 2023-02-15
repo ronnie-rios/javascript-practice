c5e6aa5c06973c5010d5b12d0c7f6657ab7beeff
Ronnie Rios <ronniesmail14@gmail.com>
2023-02-15 10:58:41 -0600

    group anagram

---
+++ algo2/groupAnagrams.js
@@ -0,0 +1,14 @@
+// Input: strs = ["eat","tea","tan","ate","nat","bat"]
+strs = ["eat","tea","tan","ate","nat","bat"]
+// Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+function groupAnagrams(str){
+    let map ={} //charCount to list of anagrams
+    
+    for(let i=0;i<str.length;i++) {
+
+    }
+}
+
+
+groupAnagrams(strs)
