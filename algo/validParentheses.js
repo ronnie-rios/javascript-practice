50a47ebb425d67008ea432ed64096f6a9ef92022
Ronnie Rios <ronniesmail14@gmail.com>
2023-02-20 18:11:15 -0600

    alt solution to variation

--- algo/validParentheses.js
+++ algo/validParentheses.js
@@ -27,3 +27,14 @@ const validParentheses = function(s) {
      return stack.length === 0;
    
 };
+// for only parentheses not {} || []
+function validParentheses(parens){
+    var n = 0;
+    for (var i = 0; i < parens.length; i++) {
+      if (parens[i] == '(') n++;
+      if (parens[i] == ')') n--;
+      if (n < 0) return false;
+    }
+    
+    return n == 0;
+  }
