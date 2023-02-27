89223724e237db18083ec9e3d300670fc9e1db6d
Ronnie Rios <ronniesmail14@gmail.com>
2023-02-27 17:31:56 -0600

    findodd

--- algo2/fb.js
+++ algo2/fb.js
@@ -1,20 +1,6 @@
 
-function fizzBuzz(number) {
-    for(let i=1; i <= number; i++) {
-       //multiples of 3 AND 5
-       //multiples of 3
-       //multiples of 5
-       //log the number (i)
-       if (i % 3 === 0 && i % 5 === 0) {
-            console.log('fizzbuzz');
-       } else if (i % 3 === 0) {
-            console.log('fizz');
-       } else if (i % 5 === 0) {
-            console.log('buzz');
-       } else {
-        console.log(i);
-       }
-    }
+function findOdd(num) {
+     console.log(num);
 }
 
-fizzBuzz(100)
+const arr = [1,2,3,4,5,6,]
