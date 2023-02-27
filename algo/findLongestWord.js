d77d31bfaa121787dfb43d9070e237b04d1ec942
Ronnie Rios <ronniesmail14@gmail.com>
2023-02-27 17:28:48 -0600

    practice

--- algo/findLongestWord.js
+++ algo/findLongestWord.js
@@ -7,13 +7,13 @@ function findLongestWordLength(str) {
     for (let i =0; i <words.length; i++){
         //check the length to our compare
       if (words[i].length > maxLength) {
-        //set maxlength to words arr lengtha at each length
+        //set maxlength to words arr length at each length
         maxLength = words[i].length
-        console.log(maxLength)
+        
       }
     }
     return maxLength
   
   }
   
-  findLongestWordLength("The quick brown fox jumped over the lazy dog");
+  console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"))
