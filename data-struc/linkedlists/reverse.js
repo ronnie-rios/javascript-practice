3465741d4e483b36877d529c0c914679f15b1409
Ronnie Rios <ronniesmail14@gmail.com>
2023-04-06 13:23:17 -0500

    added

---
+++ data-struc/linkedlists/reverse.js
@@ -0,0 +1,17 @@
+function reverseLinkList(head) {
+    let prev = null
+    let next = null
+    //as long as head does not equal null
+    while(head != null) {
+        //assign next to head
+        next = head.next
+        //then move it over to the prev
+        head.next = prev
+        //assign prev the current value
+        prev = head
+        //point to the next head
+        head = next
+    }
+    return prev
+}
+
