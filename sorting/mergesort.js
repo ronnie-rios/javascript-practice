824eb6df968b60117d129e073bb2cae40ad5c38f
Ronnie Rios <ronniesmail14@gmail.com>
2023-05-05 11:04:29 -0500

    merge sort

--- sorting/mergesort.js
+++ sorting/mergesort.js
@@ -1,35 +1,38 @@
 const nums = [4, 1, 3, 2, 6, 5];
-
-function partition(arr) {
-  let length = arr.length;
-  //basecase
-  if (length <= 1) {
-    return;
+const nums2 = [1,2,3,4,5]
+function mergeSort(arr) {
+  // base case
+  if (arr.length <= 1) {
+    return arr;
   }
 
-  //get middle
-  let middle = length / 2;
+  // divide array into halves
+  const middleIndex = Math.floor(arr.length / 2);
+  const leftArr = arr.slice(0, middleIndex);
+  const rightArr = arr.slice(middleIndex);
 
-  const left = arr.slice(0, mid);
-  const right = arr.slice(mid);
-
-  return merge(mergeSort(left), mergeSort(right));
+  // recursively sort and merge the halves
+  return merge(
+    mergeSort(leftArr),
+    mergeSort(rightArr)
+  );
 }
 
-function merge(left, right) {
-  const result = [];
-  let i = 0;
-  let j = 0;
+function merge(leftArr, rightArr) {
+  let result = [];
 
-  while (i < left.length && j < right.length) {
-    if (left[i] < right[j]) {
-      result.push(left[i]);
-      i++;
+  while (leftArr.length && rightArr.length) {
+    if (leftArr[0] < rightArr[0]) {
+      //add the left sided value if it's smaller
+      result.push(leftArr.shift());
     } else {
-      result.push(right[j]);
-      j++;
+      //add the right sided value if it's smaller
+      result.push(rightArr.shift());
     }
   }
 
-  return result.concat(left.slice(i)).concat(right.slice(j));
+  // add remaining elements to result array
+  return [...result, ...leftArr, ...rightArr];
 }
+
+console.log(merge(nums, nums2));
