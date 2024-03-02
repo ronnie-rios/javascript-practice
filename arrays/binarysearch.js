f450c2ec293b7fbf71ef1621238aa91d3b16bf5e
Ronnie Rios <ronniemarkrios@gmail.com>
2024-03-02 08:00:14 -0600

    binarysearch

--- arrays/binarysearch.js
+++ arrays/binarysearch.js
@@ -1,3 +1,19 @@
-function binarySearch() {}
+function binarySearch(arr, target) {
+  let left = 0;
+  let right = arr.length -1;
+  while(left <= right) {
+    let middle = Math.floor((left+right)/2)
+
+    if(arr[middle]===target) {
+      return middle;
+    } else if (arr[middle] < target) {
+      left = middle+1
+    } else {
+      right = middle -1
+    }
+  }
+  return -1
+
+}
 function binarySearch2() {}
 function binarySearch3() {}
