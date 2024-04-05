9ca5cd5714c88b1e4b2a9aa7a48a71bc58c94542
Ronnie Rios <ronniemarkrios@gmail.com>
2024-04-05 15:23:17 -0500

    sorting prac

--- sorting/bubblesort.js
+++ sorting/bubblesort.js
@@ -14,5 +14,17 @@ function bubbleSort(arr) {
   }
 }
 
+function bubSort(nums) {
+  for(let i =0; i < nums.length; i++) {
+    for (letj=1; j <nums.length;j++) {
+      if(nums[j] > nums[i]) {
+        let temp = nums[j] // curr
+        nums[j] = nums[j + 1]
+        nums[j+1] = temp;
+      }
+    }
+  }
+}
+
 bubbleSort(numbers);
 console.log(numbers);
