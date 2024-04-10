e8b373b55e96c958aa9b4f24885825e9b9c01aab
Ronnie Rios <ronniemarkrios@gmail.com>
2024-04-10 18:14:46 -0500

    binary seach

--- algo/binarysearch2.js
+++ algo/binarysearch2.js
@@ -14,3 +14,23 @@ function binarySeach(target, start, end) {
         return binarySeach(target, middle +1, end)
     }
 }
+
+
+function bSearch(target, start, end) {
+    let start;
+    let end = nums.length - 1;
+
+    while(start<=end) {
+        let middle = Math.floor((start + end) / 2);
+
+        if(target ===nums[middle]) {
+            return middle;
+        } else if (nums[middle] < target) {
+            start = middle + 1;
+        } else {
+            end = middle -1 ;
+        }
+    }
+
+    return -1;
+}
