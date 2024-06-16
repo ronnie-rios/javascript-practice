775c5b09e475302b7efd2ec8c5207b13fe84a51d
Ronnie Rios <ronniemarkrios@gmail.com>
2024-06-15 23:15:34 -0500

    binary search prac

--- algo/binarysearch2.js
+++ algo/binarysearch2.js
@@ -45,3 +45,20 @@ function newFunc(param) {
 
     return newArr
 }
+
+function testBinarySearch(target, start, end) {
+    let start;
+    if(start > end) {
+        return 'not found'
+    }
+    const middle =Math.floor((start +end) /2);
+    if(arr[middle]===target) {
+        return `found at ${middle}`;
+    }
+    if(arr[middle] > target) {
+        return binarySeach(target, start, middle - 1);
+    }
+    if (arr[middle] < target) {
+        return binarySeach(target, middle +1, end)
+    }
+}
