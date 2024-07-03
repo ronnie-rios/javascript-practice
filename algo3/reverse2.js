f9563b43458165d8504ef99c6e53d570dc3efe77
Ronnie Rios <ronniemarkrios@gmail.com>
2024-07-02 22:39:39 -0500

    reverse

--- algo3/reverse2.js
+++ algo3/reverse2.js
@@ -1,5 +1,13 @@
 const arr = [1,2,3,4,5]
 
 function reverseArr(arr) {
-  
+  const newArr = [];
+
+  for(let i = arr.length -1; i >=0; i--) {
+    newArr.push(arr[i])
+  }
+  return newArr;
 }
+
+const example = reverseArr(arr);
+console.log(example);
