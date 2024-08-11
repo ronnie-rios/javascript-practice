f2017ad6abd8a1ef11093e7bbd47c993fe4bb91b
Ronnie Rios <ronniemarkrios@gmail.com>
2024-08-10 19:42:50 -0500

    return word plust index

--- algo/findLongestWord.js
+++ algo/findLongestWord.js
@@ -3,17 +3,19 @@ function findLongestWordLength(str) {
     let words = str.split(' ')
     //have something to compare to
     let maxLength = 0
+    let longestWordIndex = 0;
     //loop through the arr
     for (let i =0; i <words.length; i++){
         //check the length to our compare
       if (words[i].length > maxLength) {
         //set maxlength to words arr length at each length
         maxLength = words[i].length
-        
+        longestWordIndex = i
       }
     }
-    return maxLength
-  
+    return [maxLength, words[longestWordIndex]]
+    
   }
   
   console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"))
+  console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog but the dog starts Jumping"))
