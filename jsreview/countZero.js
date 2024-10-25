15c660d65dc05bf42ad44ba7e114f0dbd520575e
Ronnie Rios <ronniemarkrios@gmail.com>
2024-10-25 15:04:56 -0500

    something like this

--- jsreview/countZero.js
+++ jsreview/countZero.js
@@ -1,1 +1,12 @@
 const nums = [ 10, 100, 200, 3000, 5000] //return the amount of numbers that have odd num of digits
+
+function countTheZero(nums) {
+  let count;
+
+  for(let i=0; i<nums.length;i++) {
+    let num = nums[i].split('');
+    for(let j =0; j< num[j]; j++) {
+      num[j] 
+    }
+  }
+}
